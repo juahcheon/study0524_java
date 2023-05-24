2ce393252d8f56f0f74bcd8b810601082159b74a
juahcheon <juacheon@gmail.com>
2023-05-24 17:39:08 +0900

    study_0524

---
+++ src/com/mycompony/study/ch02/Exc0524.java
@@ -0,0 +1,23 @@
+package com.mycompony.study.ch02;
+
+public class Exc0524 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+		boolean b = false;
+		
+		int c = 1;
+		int d;
+		d = 2;
+		
+		int e,f,g;
+		e = 3;
+		
+		System.out.println("e : " + e);
+	}
+
+}
--- src/com/mycompony/study/ch02/Helloworld.java
+++ src/com/mycompony/study/ch02/Helloworld.java
@@ -9,6 +9,21 @@ public class Helloworld {
 		System.out.println("나이 : 28세");
 		System.out.println("성별 : 여");
 		
+		// 변수 선언 : 데이터타입 변수명;
+		// 변수 초기화 : 데이터타입 변수명 = 리터럴;
+		
+		boolean a = true;
+		boolean b = false;
+		
+		int c = 1;
+		int d;
+		d = 5;
+		
+		int e,f,g;
+		e = 10;
+		
+		System.out.println("e : " + e);
+		
 	}
 
 }
